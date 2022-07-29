30c9ea04937b99aa0014c7fecc0f277f91fe54bf
xowar <marioxpen@gmail.com>
2022-07-29 06:44:55 -0600

    Inicio de proyecto

---
+++ practica 1/index.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Respuestas</title>
+	</head>
+	<body>
+		<div>
+			<form method="GET" action="">
+				<h4>Aplicación web en PHP</h4>
+				<input type="submit" name="">
+			</form>
+		</div>
+
+
+	</body>
+</html>
+
+
